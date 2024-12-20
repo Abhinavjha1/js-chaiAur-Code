838bfe5974b784acad2c98f7dcb9c3b96bf41a29
Abhinavjha1 <46905723+Abhinavjha1@users.noreply.github.com>
2024-12-20 18:32:24 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 12345
+let accountName = "Abhinav"
+var accountEmail = "abhinav@gmail.com"
+accountCity = "JSR"
+
+// accountId = "212212" //  ctrl+slash for comment
+
+// console.log(accountId);
+
+console.table([accountEmail,accountId,accountName,accountCity])
+
+// use console.table for accesing all values at once
+
+accountEmail = "aj@aj.com"
+accountName = "Anand"
+accountCity = "DSS"
+
+console.table([accountEmail,accountId,accountName,accountCity])
+
+
+/*
+    const is used for constant values that are not changed later
+    let and var are used for varibles
+
+    do not use var because of issue in its block scope and functional scope
+
+*/
+
+
